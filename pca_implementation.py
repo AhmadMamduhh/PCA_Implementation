fcc00ccc514b76bd40d3d8d4bc0f2f9fcabf8d79
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-11-12 14:06:50 +0200

    Added initial code

---
+++ pca_implementation.py
@@ -0,0 +1,24 @@
+"""
+Created on Thu Nov 12 13:48:30 2020
+
+@author: Ahmed Mamdouh - 16P6020
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    
+    """ Main entry to the program """
+    
+    while True:
+        
+
+        restart = input('\nWould you like to restart? Enter y or n\n')
+        if restart.lower() != 'y':
+            break
+
+
+if __name__ == "__main__":
+	main()
+
